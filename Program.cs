73456635ec4a5652f58cfe00d54e5587a595ad7f
Rain Maker <108649582+KapiGameMaker@users.noreply.github.com>
2023-05-14 18:19:35 +0700

    fitst commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+  
+class GFG {
+  
+    // Main Method
+    static public void Main(String[] args)
+    {
+        while(true)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if(input < 0 )
+            {
+                Console.WriteLine("Invalid Pascal’s triangle row number.");
+            }
+            else{
+                for(int row = 0; row <= input ; row++ )
+                {
+                    for(int column = 0; column <= row; column++ )
+                    {
+                        Console.Write("{0} ",Pas(row,column));                
+                    }
+                    Console.WriteLine();
+                }
+                break;
+            }
+        }        
+    }
+
+    static public int Pas(int row, int column)
+    {
+        return Fac(row!) / (Fac(row - column) * Fac(column) ) ;
+    }
+
+    static public int Fac(int number)
+    {
+        if(number <= 1) return 1;
+        return number * Fac(number - 1);
+    }
+}
